5f937e5892856419dd946d67821f95d72b00ab1f
Jonay024 <jonay024@gmail.com>
2020-04-20 12:51:52 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "base-orange": "#d29f68",
+        "plus-orange": "#cc8d66",
+        "base-black": "#232220",
+        "base-transparent": "rgba(255, 255, 255, 0.35)",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
